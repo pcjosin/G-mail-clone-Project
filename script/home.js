93761ca8ce7b51db8ab29f7fa297a9b80dce145d
Vighnesh A K <vighnesh.ak@experionglobal.local>
2023-12-23 13:53:41 +0530

    fixed function retrieve email body in html format

--- script/home.js
+++ script/home.js
@@ -1,4 +1,3 @@
-
 let emailListContainer;
 let emailBody;
 let emailSubject;
@@ -22,7 +21,8 @@ const API_KEY = "AIzaSyAGzP3_IUN8Ds05jBNckdYrFR6jyDeoeEo";
 loadPage2Content();
 
 // Initialize the Gmail API client on the Next Page
-const accessToken = localStorage.getItem("accessToken");
+const accessToken = localStorage.getItem("accessToken"); //+++++++++++++++++++++++++++++++++++++++++++++++ change access token to session storage
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ find a way to encrypt access token
 
 // Initialize the Gmail API client on the Next Page
 function gapiLoaded() {
@@ -41,7 +41,7 @@ function gapiLoaded() {
 
           // Call the listLabels function
           listLabels();
-          listLatestEmails(10);
+          listLatestEmails(20);
         } else {
           document.getElementById("nextpage-content").innerText =
             "Access token not found.";
@@ -61,18 +61,11 @@ async function listLatestEmails(numberOfEmails) {
     console.log(response.result.messages);
 
     for (const message of response.result.messages) {
-      const senderNameContent = await getSenderName(message.id);
-      console.log(senderNameContent);
-      const previewTitleContent = await getSendSubject(message.id);
-      const previewTextContent = await getMessageSnippet(message.id);
-      const sendTimeContent = await getSendTime(message.id);
+      const messagePreview = await getEmailPreview(message.id); // calling function to get a preview of email
+      console.log(messagePreview);
 
-      const emailListElement = loadEmailContent(
-        senderNameContent,
-        previewTitleContent,
-        previewTextContent,
-        sendTimeContent
-      );
+      const emailListElement = loadEmailContent(messagePreview); //calling function to generate an email preview element
+
       emailListElement.setAttribute("id", message.id);
       emailListElement.onclick = () => clickHandle(message.id);
 
@@ -82,6 +75,26 @@ async function listLatestEmails(numberOfEmails) {
     console.error("Error listing emails:", error);
   }
 }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// function to return a preview of email
+async function getEmailPreview(messageId) {
+  console.log("inside preview");
+  try {
+    const response = gapi.client.gmail.users.messages.get({
+      userId: "me",
+      id: messageId,
+      format: "metadata",
+      metadataHeaders: ["Subject", "From","Date"],
+    });
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error("Error getting email preview:", error);
+  }
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 async function getSenderName(messageId) {
   try {
@@ -160,14 +173,16 @@ async function clickHandle(emailElementId) {
     .catch((error) => console.error("Error:", error));
 
   emailSubjectContent = await getSendSubject(emailElementId);
-  emailBodyContent = await getEmailBodyText(emailElementId);
+  emailBodyContent = await getEmailBodyHtml(emailElementId);
+  console.log(emailBodyContent + "))))))))");
 
   // Update your HTML elements
   emailSubject.innerText = emailSubjectContent;
   emailBody.innerHTML = emailBodyContent;
+  emailBody.style.overflowY = "scroll";
 }
 
-async function getEmailBodyText(messageId) {
+async function getEmailBodyHtml(messageId) {
   try {
     // Use the Gmail API to get the email content
     const response = await gapi.client.gmail.users.messages.get({
@@ -182,28 +197,37 @@ async function getEmailBodyText(messageId) {
     if (emailData.payload && emailData.payload.parts) {
       // Iterate through parts
       for (const part of emailData.payload.parts) {
-        // Check if the part is text/plain (plain text)
+        // Check if the part is text/html (HTML content)
         if (
           part.mimeType === "text/html" &&
           part.body &&
           part.body.data
         ) {
-          // Decode the base64-encoded data
-          const bodyText =atob(decodeURIComponent(part.body.data.replace(/=([\s\S]{2})/g, '%$1')));
-          console.log(bodyText)
-
-          // Return the decoded body text
-          return bodyText;
+          // Decode the base64-encoded data using custom function
+          console.log(part.body.data)
+          const htmlContent =  decodeURIComponent(escape(atob(part.body.data.replace(/\-/g, '+').replace(/\_/g, '/'))));
+          console.log(htmlContent)
+          // Return the decoded HTML content
+          return htmlContent;
         }
       }
     }
 
-    // Return an empty string if no valid body text is found
+    // Return an empty string if no valid HTML content is found
     return "";
   } catch (error) {
-    console.error("Error getting email body text:", error);
+    console.error("Error getting email HTML content:", error);
     return ""; // Return an empty string in case of an error
   }
+}
+
+function decodeBase64(encodedString) {
+  const binaryString = atob(encodedString);
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+  }
+  return String.fromCharCode.apply(null, bytes);
 }
 
 async function getSenderEmail(messageId) {
@@ -302,7 +326,7 @@ async function listLabels() {
     labelDiv.classList.add("labelDiv", "row", "ms-2", "me-1", "p-2");
 
     let iconDiv = document.createElement("div"); //adding icon tag
-    iconDiv.classList.add("bi", "col-1", "fs-3");
+    iconDiv.classList.add("bi", "col-1", "fs-6", "me-2");
     switch (labels[i].id.toLowerCase()) {
       case "chat":
         iconDiv.classList.add("bi-chat-left-text");
@@ -353,7 +377,7 @@ async function listLabels() {
     let aName = labels[i].name.toLowerCase();
     aName = aName.charAt(0).toUpperCase() + aName.slice(1);
     anchor.innerHTML = aName;
-    anchor.classList.add("col-6", "labelAnchor", "fs-4");
+    anchor.classList.add("col-6", "labelAnchor", "fs-6");
     labelDiv.append(anchor);
 
     // labelDiv.innerHTML=labels[i].name;
@@ -363,13 +387,61 @@ async function listLabels() {
   console.log(labels);
 }
 
-function loadEmailContent(
-  senderNameContent,
-  previewTitleContent,
-  previewTextContent,
-  sendTimeContent
-) {
+function loadEmailContent(response) {
+
+
+  var shortMonthNames = [
+'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+];
+
+
+  const previewTitleContent = response.result.payload.headers.find(
+    (header) => header.name === "Subject"
+  ).value;
+  const senderNameContent =
+    response.result.payload.headers
+      .find((header) => header.name === "From")
+      .value.split("<")[0] == ""
+      ? response.result.payload.headers.find(
+          (header) => header.name === "From"
+        ).value
+      : response.result.payload.headers
+          .find((header) => header.name === "From")
+          .value.split("<")[0];
+  const sndDateTime =new Date(response.result.payload.headers.find((header) => header.name === "Date").value);
+  console.log('snddte:'+sndDateTime);
+  let today = new Date();
+  console.log('snddte:'+today);
+
+  if (today.getFullYear()===sndDateTime.getFullYear() &&
+      today.getMonth()===sndDateTime.getMonth() &&
+      today.getDate()===sndDateTime.getDate()){
+          var sendTimeContent=sndDateTime.toLocaleTimeString().replace(/:\d{2} /, ' ');
+                  console.log('snddte:'+sendTimeContent);
+      }
+  else if(today.getFullYear() ===sndDateTime.getFullYear()){
+      var sendTimeContent=   shortMonthNames[parseInt( sndDateTime.getMonth())] +' '+ sndDateTime.getDate();
+      console.log('snddte:'+sendTimeContent);
+
+  }
+  else{
+      var sendTimeContent=sndDateTime.toLocaleDateString();
+      console.log('snddte:'+sendTimeContent);
+
+  }
+
+  
+
+ // const sendTimeContent 
+
+
+  const previewTextContent = response.result.snippet;
+  const labels = response.result.labelIds;
+
   console.log("function running");
+
+  //+++++++++++++++++++++++++++++++++++ code to test generation++++++++++++++++++++++++++
 
   // let senderNameContent = 'sender name';
   // let previewTitleContent = 'this is preview title';
@@ -377,6 +449,7 @@ function loadEmailContent(
   // let sendTimeContent = '09:23 pm';
 
   // for (let i = 0; i < 30; i++) {
+  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   const emailPreviewBar = document.createElement("div");
   emailPreviewBar.classList.add("preview-bar");
@@ -400,7 +473,13 @@ function loadEmailContent(
   const star = document.createElement("div");
 
   star.classList.add("list-star");
-  star.innerHTML = '<i class="bi bi-star check-star-flag"></i>';
+
+  if (labels.includes("STARRED")) {
+    star.innerHTML =
+      '<i class="bi bi-star-fill check-star-flag" style="color:yellow"></i>';
+  } else {
+    star.innerHTML = '<i class="bi bi-star check-star-flag"></i>';
+  }
 
   leftDiv.appendChild(star);
 
@@ -408,7 +487,13 @@ function loadEmailContent(
 
   important.classList.add("list-important");
 
-  important.innerHTML = '<i class="bi bi-flag check-star-flag"></i>';
+  if (labels.includes("IMPORTANT")) {
+    important.innerHTML =
+      '<i class="bi bi-caret-right-fill check-star-flag" style="color:yellow"></i>';
+  } else {
+    important.innerHTML =
+      '<i class="bi bi-caret-right check-star-flag"></i>';
+  }
 
   leftDiv.appendChild(important);
 
@@ -418,11 +503,17 @@ function loadEmailContent(
   const sendertext = document.createElement("span");
   sendertext.classList.add("sendertext");
 
+  if (labels.includes("UNREAD")) {
+    sendertext.classList.add("bold-text");
+  }
+
   sendertext.innerText = senderNameContent;
   sender.appendChild(sendertext);
   leftDiv.appendChild(sender);
 
   centralLineDiv.appendChild(leftDiv);
+
+  //right section
 
   const rightDiv = document.createElement("div");
   rightDiv.classList.add("right-div", "col-md-8", "d-flex");
@@ -432,6 +523,9 @@ function loadEmailContent(
 
   const previewTitle = document.createElement("div");
   previewTitle.classList.add("previewTitle");
+  if (labels.includes("UNREAD")) {
+    previewTitle.classList.add("bold-text");
+  }
 
   previewTitle.innerText = previewTitleContent + " - ";
 
@@ -445,8 +539,12 @@ function loadEmailContent(
   rightDiv.appendChild(preview);
 
   const sendTime = document.createElement("div");
-  sendTime.classList.add("preview-time", "col-md-3", "col-sm-3");
+  sendTime.classList.add("preview-time", "col-md-3", "col-sm-3","pe-4");
+  
   const sendTimeinside = document.createElement("span");
+  if (labels.includes('UNREAD')){
+      sendTimeinside.classList.add("bold-text");
+  }
   sendTimeinside.innerText = sendTimeContent;
 
   sendTime.appendChild(sendTimeinside);
