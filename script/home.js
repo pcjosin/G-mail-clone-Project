f016185e02640e7f139e7d6419a5d4172e0746a0
Vijay R <vijay.r@experionglobal.local>
2024-01-08 22:11:00 +0530

    feat:typsecript of app drawer open function

--- script/home.js
+++ script/home.js
@@ -174,28 +174,28 @@ document.addEventListener("click", function (event) {
 
 //app drawer display
 
-let menuIcon = document.getElementById("icon-menu");
-let appDivOpen = document.getElementById("app-drawer-expand-container");
-menuIcon.onclick = () => {
-  appDivOpen.style.display = 'block';
-  console.log("App drawer clicked");
-};
+// let menuIcon = document.getElementById("icon-menu");
+// let appDivOpen = document.getElementById("app-drawer-expand-container");
+// menuIcon.onclick = () => {
+//   appDivOpen.style.display = 'block';
+//   console.log("App drawer clicked");
+// };
 
-//hide the app drawer when clicked outside
+// //hide the app drawer when clicked outside
 
 
-document.addEventListener("click", function (event) {
-  // Check if the clicked element is NOT the div or a child of the div
-  if (
-    event.target !== appDivOpen &&
-    !appDivOpen.contains(event.target) &&
-    event.target !== menuIcon &&
-    !menuIcon.contains(event.target)
-  ) {
-    // Hide the div
-    appDivOpen.style.display = 'none';
-  } 
-});
+// document.addEventListener("click", function (event) {
+//   // Check if the clicked element is NOT the div or a child of the div
+//   if (
+//     event.target !== appDivOpen &&
+//     !appDivOpen.contains(event.target) &&
+//     event.target !== menuIcon &&
+//     !menuIcon.contains(event.target)
+//   ) {
+//     // Hide the div
+//     appDivOpen.style.display = 'none';
+//   } 
+// });
 
 
 
